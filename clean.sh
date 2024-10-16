0928967e6242d8a8c3fd7fbb1fb9acd648fe0465
JM <jay.manvirk@gmail.com>
2024-10-16 13:09:03 +0000

    removed install lines

--- clean.sh
+++ clean.sh
@@ -9,10 +9,6 @@ echo "Removing unnecessary applications..."
 sudo apt remove --purge -y gnome-software
 sudo apt autoremove -y
 
-# Install SD card drivers for Mac hardware
-echo "Installing SD card drivers..."
-sudo apt install -y exfat-fuse exfat-utils
-
 # Improve boot-up speed
 echo "Improving boot-up speed..."
 # Lower the timeout grub configuration
